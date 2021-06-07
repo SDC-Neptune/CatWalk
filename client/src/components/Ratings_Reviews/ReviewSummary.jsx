4cfcf1549e87235973edd4605f99f06864435e61
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-06-07 14:06:47 -0500

    Finished review summary layout

--- client/src/components/Ratings_Reviews/ReviewSummary.jsx
+++ client/src/components/Ratings_Reviews/ReviewSummary.jsx
@@ -2,8 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 const RatingsReviews = () => {
 
-  const [rating, setRating] = useState(4.25);
+  const [rating, setRating] = useState(4.2);
   const [percentage, setPercent] = useState(85);
+  const [recommend, setRecommed] = useState(100);
+  const [fiveStar, setfiveStar] = useState(55);
+  const [fourStar, setfourStar] = useState(0);
+  const [threeStar, sethreeStar] = useState(20);
+  const [twoStar, settwoStar] = useState(6);
+  const [oneStar, setoneStar] = useState(1);
+  const [totalStars, setTotalStars] = useState(
+    (fiveStar + fourStar + threeStar + twoStar + oneStar)
+  );
 
   return (
     <div>
@@ -24,30 +33,67 @@ const RatingsReviews = () => {
         <div className="star-ratings-css-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
       </div>
       <div>
-        100% of reviews recommend this product
+        {recommend}% of reviews recommend this product
       </div>
       <div className='test'>
-        <span className='reviewBarText'>5 stars</span> <progress value="70" max="100"></progress>
+        <span className='reviewBarText'>5 stars</span> <progress value={(fiveStar / totalStars) * 100} max={totalStars}></progress><span>{fiveStar}</span>
         <br/>
-        <span className='reviewBarText'>4 stars</span> <progress value="33" max="100"></progress>
+        <span className='reviewBarText'>4 stars</span> <progress value={(fourStar / totalStars) * 100} max={totalStars}></progress><span>{fourStar}</span>
         <br/>
-        <span className='reviewBarText'>3 stars</span> <progress value="99" max="100"></progress>
+        <span className='reviewBarText'>3 stars</span> <progress value={(threeStar / totalStars) * 100} max={totalStars}></progress><span>{threeStar}</span>
         <br/>
-        <span className='reviewBarText'>2 stars</span> <progress value="8" max="100"></progress>
+        <span className='reviewBarText'>2 stars</span> <progress value={(twoStar / totalStars) * 100} max={totalStars}></progress><span>{twoStar}</span>
         <br/>
-        <span className='reviewBarText'>1 stars</span> <progress value="0" max="100"></progress>
+        <span className='reviewBarText'>1 stars</span> <progress value={(oneStar / totalStars) * 100} max={totalStars}></progress><span>{oneStar}</span>
       </div>
+      <br></br>
       <div >
-        Size
         <div>
-          <input type="range" min="1" max="100" value="0" name="tooSmall"></input>
-          <label htmlFor="tooSmall">Too Small</label>
+          <b>Size</b>
+          <div>
+            <input className='feedbackChars' type="range" min="0" max="5" value="2.5" name="tooSmall"></input>
+            <div className='parent'><span className='r1'>Too small</span> <span className='r2'>Perfect</span> <span className='r3'>Too wide</span></div>
+          </div>
         </div>
-        Comfort
+        <br></br>
         <div>
-          <label htmlFor="poor">Poor</label>
-          <input type="range" min="1" max="100" value="55" name="poor"></input>
-          <label htmlFor="poor">Perfect</label>
+          <b>Comfort</b>
+          <div>
+            <input className='feedbackChars' type="range" min="0" max="5" value="2.7" name="poor"></input>
+            <div className='parent'><span className='r1'>Uncomfortable</span> <span className='r2'>OK</span> <span className='r3'>Perfect</span></div>
+          </div>
+        </div>
+        <br></br>
+        <div>
+          <b>Width</b>
+          <div>
+            <input className='feedbackChars' type="range" min="0" max="5" value="2" name="poor"></input>
+            <div className='parent'><span className='r1'>Too narrow</span> <span className='r2'>Perfect</span> <span className='r3'>Too wide</span></div>
+          </div>
+        </div>
+        <br></br>
+        <div>
+          <b>Quality</b>
+          <div>
+            <input className='feedbackChars' type="range" min="0" max="5" value="4" name="poor"></input>
+            <div className='parent'><span className='r1'>Poor</span> <span className='r2'>OK</span> <span className='r3'>Perfect</span></div>
+          </div>
+        </div>
+        <br></br>
+        <div>
+          <b>Fit</b>
+          <div>
+            <input className='feedbackChars' type="range" min="0" max="5" value="1.3" name="poor"></input>
+            <div className='parent'><span className='r1'>Too tight</span> <span className='r2'>Perfect</span> <span className='r3'>Too loose</span></div>
+          </div>
+        </div>
+        <br></br>
+        <div>
+          <b>Length</b>
+          <div>
+            <input className='feedbackChars' type="range" min="0" max="5" value="4.9" name="poor"></input>
+            <div className='parent'><span className='r1'>Runs short</span> <span className='r2'>Perfect</span> <span className='r3'>Runs Long</span></div>
+          </div>
         </div>
       </div>
     </div>
