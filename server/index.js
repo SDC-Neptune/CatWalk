7c3c0137b1249b8e5ef46069fd066a86b38f955b
Scott Provence <scott.provence@yahoo.com>
2021-06-07 10:42:24 -0400

    removed cache

--- server/index.js
+++ server/index.js
@@ -7,44 +7,15 @@ const axios = require('axios');
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 
-app.get('/api/styles', function (req, res) {
-
-  let options = {
+app.all('/*', (req, res, next) => {
+  axios({
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe' + req.url,
+    method: req.method,
     headers: {
-      'Authorization': `${config.TOKEN}`
-    }
-  };
-
-  var productId = req.query.id;
-
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${productId}/styles`, options)
-    .then((styles) => {
-      res.json(styles.data);
-    })
-    .catch((error) => {
-      res.json(error);
-    });
-
-});
-
-app.get('/api/product', function (req, res) {
-
-  let options = {
-    headers: {
-      'Authorization': `${config.TOKEN}`
-    }
-  };
-
-  var productId = req.query.id;
-
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${productId}`, options)
-    .then((product) => {
-      res.json(product.data);
-    })
-    .catch((error) => {
-      res.json(error);
-    });
-
+      Authorization: config.TOKEN
+    },
+    body: req.body
+  }).then((response) => res.send(response.data)).catch(err => console.log(err));
 });
 
 app.listen(PORT, () => {
