710553ae951579d8ceaaa0e09e70397d83bb274e
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-06-12 09:11:38 -0500

    Added no option for helpfulness review

--- client/src/components/Ratings_Reviews/IndivReview.jsx
+++ client/src/components/Ratings_Reviews/IndivReview.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Stars from './Stars.jsx';
 import Recommend from './Recommend.jsx';
+import PicModal from './PicModal.jsx';
 
 const IndivReview = ({detail}) => {
   const [recommend, setRecommend] = useState(detail.recommend ? true : false);
   const [isResponse, setIsResponse] = useState(detail.response ? true : false);
   const [helpful, setHelpful] = useState(Number(detail.helpfulness));
+  const [noHelpful, setNoHelpful] = useState(0);
   const [disabled, setDisabled] = useState(false);
   const dateTimeFormat = new Date(detail.date);
 
@@ -29,8 +31,13 @@ const IndivReview = ({detail}) => {
         <br></br>
         {detail.response}
       </div>)}
+
       <br></br>
-      <span className='helpful'>Helpful? <u onClick={() => { !disabled ? setHelpful(helpful + 1) : null; setDisabled(true); }}>Yes</u> ({helpful}) </span> <span className='report'>Report</span>
+      <span className='helpful'> Helpful?
+        <span>  <u onClick={() => { !disabled ? setHelpful(helpful + 1) : null; setDisabled(true); }}>Yes</u>({helpful})</span>
+        <span>  <u onClick={() => { !disabled ? setNoHelpful(noHelpful + 1) : null; setDisabled(true); }}>No</u>({noHelpful})</span>
+      </span>
+      <span className='report'>Report</span>
     </div>
   );
 
---
+++ client/src/components/Ratings_Reviews/PicModal.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const PicModal = () => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+export default PicModal;
