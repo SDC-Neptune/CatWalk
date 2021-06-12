973dfe587962e77ddec9cc73f1821d369c5745f7
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-06-12 17:18:12 -0500

    Added get requests to review detail

--- client/src/components/Ratings_Reviews/PicModal.jsx
+++ client/src/components/Ratings_Reviews/PicModal.jsx
@@ -4,10 +4,10 @@ import React, { useState, useEffect } from 'react';
 const PicModal = ({image, handleChange}) => {
 
   return (
-    <div className='modal-overlay'>
-      <div className='modal'>
+    <div className='modal-overlay-reviews'>
+      <div className='modal-reviews'>
         <button className='reviewButton' onClick={handleChange}>Close</button>
-        <img src={image} alt='Product review image' />
+        <img src={image} alt='Product review image' height='700'/>
       </div>
     </div>
   );
